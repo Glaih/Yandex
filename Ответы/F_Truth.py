9955a83d2f577569d958512ddcfb140588db809f
Naryushkin Konstantin <glaih2k@gmail.com>
2021-03-24 11:32:07 +0300

    Reworked F

--- Ответы/F_Truth.py
+++ Ответы/F_Truth.py
@@ -1,4 +1,3 @@
-import math
 import csv
 
 import requests
@@ -22,47 +21,41 @@ class DataParser:
 
     def _get_data(self):
         host_url = f'http://{self.host}:{self.port}/data'
-        # data = requests.get(host_url).json()['data']
-        data = [{"electric": [62, 45, 15, 44, 20], "magnetic": [47, 10, 43, 61], "gravitational": [5, 41, 63, -9, 9, 12]},
-                     {"electric": [1, 82, 56, 48], "magnetic": [-2, 58, 96, 14, 94], "gravitational": [-8, 98, -5]},
-                     {"electric": [68, 81, 40, -2, -8], "magnetic": [74, -12, 67, 54], "gravitational": [85, -6]}]
+        data = requests.get(host_url).json()['data']
+        # data = [{"electric": [62, 45, 15, 44, 20], "magnetic": [47, 10, 43, 61], "gravitational": [5, 41, 63, -9, 9, 12]},
+        #              {"electric": [1, 82, 56, 48], "magnetic": [-2, 58, 96, 14, 94], "gravitational": [-8, 98, -5]},
+        #              {"electric": [68, 81, 40, -2, -8], "magnetic": [74, -12, 67, 54], "gravitational": [85, -6]}]
         return data
 
     def process(self, write=True):
-        result = defaultdict(lambda: {
-            'max': -math.inf,
-            'min': math.inf,
-            'avg': 0,
-            'sum': 0,
-            'count': 0
-        })
+        result = defaultdict(lambda: [])
         data = self._get_data()
+        count = 0
         for element in data:
+            count += 1
             for key, value in element.items():
                 for v in value:
                     if v < self.smallest or v % self.not_mult == 0:
                         continue
-                    result[key]['sum'] += v
-                    result[key]['count'] += 1
-                    if v < result[key]['min']:
-                        result[key]['min'] = v
-                    if v > result[key]['max']:
-                        result[key]['max'] = v
-
-                result[key]['avg'] = round(result[key]['sum'] / result[key]['count'], 2)
+                    result[key].append(v)
+                if count != len(element):
+                    continue
+                result[key] = [key, max(result[key]), min(result[key]), round(sum(result[key]) / len(result[key]), 2),
+                               sum(result[key])]
 
         if write:
             self._write_to_file(result)
 
         return result
 
+# If newline='' is not specified, newlines embedded inside quoted fields will not be interpreted correctly, and on
+# platforms that use \r\n lineendings on write an extra \r will be added. It should always be safe to specify newline='',
+# since the csv module does its own (universal) newline handling.
     @staticmethod
     def _write_to_file(data):
         with open('truth.csv', 'w', newline='') as f:
             writer = csv.writer(f, delimiter=';')
-            for key, value in sorted(data.items()):
-                value.pop('count')
-                writer.writerow([key, *value.values()])
+            writer.writerows(sorted(data.values()))
 
 
 acquired_data = DataParser(args.host, args.port, args.smallest, args.not_mult)
